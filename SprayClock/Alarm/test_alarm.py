3c41d8198362a6cda66f55d39defb301293cdaec
LeeneshKumar <leeneshkumar@cmail.carleton.ca>
2022-03-25 14:48:50 -0400

    Alarm code

--- SprayClock/Alarm/test_alarm.py
+++ SprayClock/Alarm/test_alarm.py
@@ -5,4 +5,31 @@ db = main.init_firebase()
 
 #testing the get_alarms() function
 
-def test_get_alarms()
+def test_get_alarms():
+	allAlarms=db.child("Set Alarms").get()
+	allData_list = allData.each()
+	options = []
+	for data in allData_list:
+                alarmtime = datetime.strptime(data.val()["Time"], "%H:%M")
+                options.append(alarmtime)
+	assert options==main.get_alarms()
+
+def test_showTime():
+	current_exp_time=main.showTime()
+   time_now = datetime.now()
+         curr_time = time_now.strftime("%H:%M")
+	assert curr_time = current_exp_time
+
+def test_ringAlarm():
+	time_now = datetime.now()
+	curre_time = time_now.strftime("%H:%M")
+	new_alarm = {"Day" : "Monday", "Time": curre_time}
+	db.child("Set Alarms").child(5).set(new_alarm)
+	expected = main.ringAlarm()
+	assert True == expected
+
+def test_buttonPressed():
+	print("button was recognized")
+	expected ==main.buttonPressed()
+	assert True == expected 
+
