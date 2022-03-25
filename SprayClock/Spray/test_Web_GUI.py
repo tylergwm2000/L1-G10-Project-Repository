d108b1d1fb507df0ed53a2c60d787484788e6fb0
tylergwm2000 <tylermak@cmail.carleton.ca>
2022-03-25 11:31:47 -0400

    [Tyler Mak] Updated test_Web_GUI to work with new frontend.py & backend.py changes

--- SprayClock/Spray/test_Web_GUI.py
+++ SprayClock/Spray/test_Web_GUI.py
@@ -1,4 +1,5 @@
-import website
+import frontend
+import backend
 import pyrebase
 from datetime import datetime
 import pytest
@@ -12,24 +13,30 @@ firebase = pyrebase.initialize_app(config)
 db = firebase.database()
 
 def test_SettingAlarmCommunication():
-	website.setButtonClicked("14:50")
+	backend.setButtonClicked("Monday", "14:50")
+	alarms = backend.get_alarms()
+	expected = "Monday 14:50"
+	assert alarms[len(alarms)-1] == expected
 	data = db.child("Subsystem Status").child("Web GUI").get()
 	expected = {"alarmTime": "14:50", "deleteAlarm": False, "setAlarm": True}
 	assert data.val() == expected
 
 def test_DeleteAlarmCommunication():
-	website.removeButtonClicked("10:20")
+	backend.removeButtonClicked("Monday 14:50")
+	alarms = backend.get_alarms()
+	for alarm in alarms:
+		assert alarm != "Monday 14:50"
 	data = db.child("Subsystem Status").child("Web GUI").get()
-	expected = {"alarmTime": "10:20", "deleteAlarm": True, "setAlarm": False}
+	expected = {"alarmTime": "14:50", "deleteAlarm": True, "setAlarm": False}
 	assert data.val() == expected
 
 def test_GetAlarmsCommunication():
-	options = website.get_alarms()
+	options = backend.get_alarms()
 	data = db.child("Set Alarms").get()
 	data_list = data.each()
 	expected = []
 	for data in data_list:
-		expected.append(datetime.strptime(data.val()["Time"], "%H:%M"))
+		expected.append(data.val()["Day"] + ' ' + data.val()["Time"])
 	assert expected == options
 
 
