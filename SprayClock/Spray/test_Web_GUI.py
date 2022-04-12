4c86d02f5961ce535c8d4b0345694e9092fa16c3
tylergwm2000 <tylermak@cmail.carleton.ca>
2022-04-12 13:30:12 -0400

    [Tyler Mak] Add comments

--- SprayClock/Spray/test_Web_GUI.py
+++ SprayClock/Spray/test_Web_GUI.py
@@ -4,6 +4,7 @@ import pyrebase
 from datetime import datetime
 import pytest
 
+#Firebase config
 config = {
 	"apiKey": "AIzaSyDo00f19D_1SEPSYfjlNw9KkVkUNf-jyLw",
 	"authDomain": "sprayclock-4e902.firebaseapp.com",
@@ -12,24 +13,27 @@ config = {
 firebase = pyrebase.initialize_app(config)
 db = firebase.database()
 
+#Testing Set Alarm button press
 def test_SettingAlarmCommunication():
-	backend.setButtonClicked("Monday", "14:50")
+	backend.setButtonClicked("Monday", "14:50") # Set with Monday and 14:50 input
 	alarms = backend.getAlarms()
 	expected = "Monday 14:50"
-	assert alarms[len(alarms)-1] == expected
+	assert alarms[len(alarms)-1] == expected # Check last alarm is the newly set alarm
 	data = db.child("Subsystem Status").child("Web GUI").get()
 	expected = {"alarmTime": "14:50", "deleteAlarm": False, "setAlarm": True}
-	assert data.val() == expected
+	assert data.val() == expected # Check Web GUI status is updated
 
+#Testing Delete Alarm button press
 def test_DeleteAlarmCommunication():
-	backend.removeButtonClicked("Monday 14:50")
+	backend.removeButtonClicked("Monday 14:50") # Delete previously set Monday 14:50 alarm
 	alarms = backend.getAlarms()
 	for alarm in alarms:
-		assert alarm != "Monday 14:50"
+		assert alarm != "Monday 14:50" # Check that alarm isn't in the list of alarms
 	data = db.child("Subsystem Status").child("Web GUI").get()
 	expected = {"alarmTime": "14:50", "deleteAlarm": True, "setAlarm": False}
-	assert data.val() == expected
+	assert data.val() == expected # Check Web GUI status is updated
 
+#Testing getting alarms from Firebase
 def test_GetAlarmsCommunication():
 	options = backend.getAlarms()
 	data = db.child("Set Alarms").get()
@@ -37,6 +41,6 @@ def test_GetAlarmsCommunication():
 	expected = []
 	for data in data_list:
 		expected.append(data.val()["Day"] + ' ' + data.val()["Time"])
-	assert expected == options
+	assert expected == options # Check get alarms method gets all the alarms
 
 
